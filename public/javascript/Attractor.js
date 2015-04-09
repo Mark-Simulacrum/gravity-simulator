c24121ac73f629b525a05aeed5f27ae66251cb8d
Mark-Simulacrum <mark.simulacrum@gmail.com>
2015-04-09 17:49:59 -0600

    Change Vector class to ES2015/ES6 format.

--- public/javascript/Attractor.js
+++ public/javascript/Attractor.js
@@ -1,26 +1,24 @@
 import * as constants from "./constants";
 
 let initNum = 1;
-function Attractor(game, center) {
-    this.type = "attractor";
-    this.id = initNum;
-    this.isAlive = true;
+export default class Attractor{
+    constructor(game, center) {
+        this.type = "attractor";
+        this.id = initNum;
+        this.isAlive = true;
 
-    this.game = game;
-    this.center = center;
-    this.mass = constants.EarthMass * 4;
+        this.game = game;
+        this.center = center;
+        this.mass = constants.EarthMass * 4;
 
-    this.radius = this.mass / constants.EarthMass * constants.EarthRadius;
+        this.radius = this.mass / constants.EarthMass * constants.EarthRadius;
 
-    initNum++;
+        initNum++;
+    }
+    calculateMass() {
+        this.mass = this.radius / constants.EarthRadius * constants.EarthMass;
+    }
+    update() {
+        this.color = `hsl(${360 / this.game.attractors.length * this.id}, 100%, 50%)`;
+    }
 }
-
-Attractor.prototype.calculateMass = function() {
-    this.mass = this.radius / constants.EarthRadius * constants.EarthMass;
-};
-
-Attractor.prototype.update = function () {
-    this.color = `hsl(${360 / this.game.attractors.length * this.id}, 100%, 50%)`;
-};
-
-export default Attractor;
