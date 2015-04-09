0f1e8a7dfeced777a420439643e831ec82c0de3e
Mark-Simulacrum <mark.simulacrum@gmail.com>
2015-04-09 17:46:31 -0600

    Change Body class to ES2015/ES6 format.

--- public/javascript/Body.js
+++ public/javascript/Body.js
@@ -4,98 +4,93 @@ import * as constants from "./constants";
 import * as pointUtils from "./pointUtils";
 
 let initNum = 0;
-function Body(game, center, isManual, options = {}) {
-    this.game = game;
-    this.center = center;
-    this.isManual = isManual;
+export default class Body {
+    constructor(game, center, isManual, options = {}) {
+        this.game = game;
+        this.center = center;
+        this.isManual = isManual;
 
-    this.isAlive = true;
-    this.type = "body";
-    this.id = initNum;
-    this.originalCenter = { x: center.x, y: center.y };
+        this.isAlive = true;
+        this.type = "body";
+        this.id = initNum;
+        this.originalCenter = { x: center.x, y: center.y };
 
-    this.mass = constants.EarthMass / 2;
-    this.radius = this.mass / constants.EarthMass * constants.EarthRadius;
+        this.mass = constants.EarthMass / 2;
+        this.radius = this.mass / constants.EarthMass * constants.EarthRadius;
 
-    let injectionVector = options.initialSpeed !== undefined ? options.initialSpeed : new Vector(
-        5500,
-        0,
-        "m/s"
-    );
-
-    this.startSpeed = injectionVector.length;
-    this.speed = injectionVector;
-    this.timeSinceUpdate = 0;
-
-    initNum++;
-}
-
-Body.prototype.acceleration = function (force) {
-    return force.length / this.mass;
-};
-
-Body.prototype.speedNow = function (speed0, force) {
-    return speed0 + this.acceleration(force) * this.timeSinceUpdate;
-};
-
-Body.prototype.distanceTravelled = function (speed) {
-    return speed * this.timeSinceUpdate;
-};
-
-Body.prototype.shift = function (speed0, force) {
-    return {
-        x: this.distanceTravelled(this.speedNow(speed0, force)) * Math.cos(force.direction),
-        y: this.distanceTravelled(this.speedNow(speed0, force)) * Math.sin(force.direction)
-    };
-};
-
-Body.prototype.update = function(timeSinceUpdate) {
-    this.timeSinceUpdate = timeSinceUpdate;
-
-    let vectors = this.game.attractors.map(attractor => {
-        let force = constants.G * this.mass * attractor.mass / Math.pow(pointUtils.distanceBetween(this.center, attractor.center), 2);
-
-        return new Vector(
-            force,
-            Math.atan2(
-                this.center.y - attractor.center.y,
-                this.center.x - attractor.center.x
-            ) + Math.PI,
-            "N"
+        let injectionVector = options.initialSpeed !== undefined ? options.initialSpeed : new Vector(
+            5500,
+            0,
+            "m/s"
         );
-    });
 
-    let planetsVector = vectors.length > 0 ? vectors.reduce((vecA, vecB) => vecA.add(vecB)) : new Vector(0, 0, "N");
-    let projectedVectors = planetsVector.projectionOnto(this.speed);
+        this.startSpeed = injectionVector.length;
+        this.speed = injectionVector;
+        this.timeSinceUpdate = 0;
 
-    let delta = {
-        a: this.shift(this.speed.length, projectedVectors.a),
-        b: this.shift(0, projectedVectors.b)
-    };
+        initNum++;
+    }
+    acceleration(force) {
+        return force.length / this.mass;
+    }
+    speedNow(speed0, force) {
+        return speed0 + this.acceleration(force) * this.timeSinceUpdate;
+    }
+    distanceTravelled(speed) {
+        return speed * this.timeSinceUpdate;
+    }
+    shift(speed0, force) {
+        return {
+            x: this.distanceTravelled(this.speedNow(speed0, force)) * Math.cos(force.direction),
+            y: this.distanceTravelled(this.speedNow(speed0, force)) * Math.sin(force.direction)
+        };
+    }
+    update(timeSinceUpdate) {
+        this.timeSinceUpdate = timeSinceUpdate;
 
-    delta.x = delta.a.x + delta.b.x;
-    delta.y = delta.a.y + delta.b.y;
+        let vectors = this.game.attractors.map(attractor => {
+            let force = constants.G * this.mass * attractor.mass / Math.pow(pointUtils.distanceBetween(this.center, attractor.center), 2);
 
-    let previousSpeed = this.speed.length;
-    this.speed = new Vector(
-        pointUtils.distanceBetween({ x: 0, y: 0 }, delta) / timeSinceUpdate,
-        Math.atan2(
-            delta.y,
-            delta.x
-        ),
-        "m/s"
-    );
+            return new Vector(
+                force,
+                Math.atan2(
+                    this.center.y - attractor.center.y,
+                    this.center.x - attractor.center.x
+                ) + Math.PI,
+                "N"
+            );
+        });
 
-    let currentAcceleration = this.speed.length - previousSpeed;
-    let min = 255;
-    let max = 360;
-    let hue = Math.min(currentAcceleration / max, 1) * (max - min) + min;
-    this.color = `hsl(${hue}, 100%, 70%)`;
+        let planetsVector = vectors.length > 0 ? vectors.reduce((vecA, vecB) => vecA.add(vecB)) : new Vector(0, 0, "N");
+        let projectedVectors = planetsVector.projectionOnto(this.speed);
 
-    this.isAlive = !pointUtils.willCollide(this.center, delta, this.game.attractors);
+        let delta = {
+            a: this.shift(this.speed.length, projectedVectors.a),
+            b: this.shift(0, projectedVectors.b)
+        };
 
-    this.center.x += delta.x;
-    this.center.y += delta.y;
-};
+        delta.x = delta.a.x + delta.b.x;
+        delta.y = delta.a.y + delta.b.y;
 
-export default Body;
+        let previousSpeed = this.speed.length;
+        this.speed = new Vector(
+            pointUtils.distanceBetween({ x: 0, y: 0 }, delta) / timeSinceUpdate,
+            Math.atan2(
+                delta.y,
+                delta.x
+            ),
+            "m/s"
+        );
+
+        let currentAcceleration = this.speed.length - previousSpeed;
+        let min = 255;
+        let max = 360;
+        let hue = Math.min(currentAcceleration / max, 1) * (max - min) + min;
+        this.color = `hsl(${hue}, 100%, 70%)`;
+
+        this.isAlive = !pointUtils.willCollide(this.center, delta, this.game.attractors);
+
+        this.center.x += delta.x;
+        this.center.y += delta.y;
+    }
+}
