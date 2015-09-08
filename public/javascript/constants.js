396dc5fabe44a15fe52e6e9773f8932dce81f4b6
Mark Simulacrum <mark.simulacrum@gmail.com>
2015-09-08 16:35:23 -0600

    Add the base functionality for the measurement display of objects.

--- public/javascript/constants.js
+++ public/javascript/constants.js
@@ -5,6 +5,23 @@ export const MetersPerPixel = EarthRadius / 10;
 export const LightSpeed = 299792458; // m/s
 export const TimeScale = 20; // Simulated seconds in real seconds.
 
+export const KilometersPerPixel = MetersPerPixel / 1000;
+export const MilesPerPixel = KilometersPerPixel * 0.62;
+export const EarthRadiiPerPixel = MetersPerPixel / EarthRadius;
+
+const selector = document.getElementById("measurement-selector");
+const options = [MilesPerPixel, KilometersPerPixel, EarthRadiiPerPixel];
+const textOptions = ["mi", "km", "earth-radii"];
+export function toDisplayUnit(pixels) {
+	const selectedIndex = selector.selectedIndex;
+
+	return pixels * options[selectedIndex];
+}
+
+export function getDisplayUnit() {
+	return textOptions[selector.selectedIndex];
+}
+
 export function toReal(pixels) {
     return pixels * MetersPerPixel;
 }
