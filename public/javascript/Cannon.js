98253cf057330a07436348a072436935426339bb
Mark Simulacrum <mark.simulacrum@gmail.com>
2015-10-07 12:55:03 -0600

    Make Cannon do less work in update() to hopefully reduce workload with large amounts of Cannons.

--- public/javascript/Cannon.js
+++ public/javascript/Cannon.js
@@ -19,22 +19,19 @@ export default class Cannon {
         this.updates = 0;
         this.rate = 50;
 
+        this.shootPointDelta = null;
         this.select(toPoint); // creates this.shootVector
 
         initNum++;
     }
     vectorToPoint() {
-        let scaledVector = this.shootVector.scale(10e3);
-
-        let vectorToPoint = {
-            x: this.center.x + scaledVector.getDeltaX(),
-            y: this.center.y + scaledVector.getDeltaY()
+        return {
+            x: this.center.x + this.shootPointDelta.x,
+            y: this.center.y + this.shootPointDelta.y
         };
-
-        return vectorToPoint;
     }
     update() {
-        if (this.updates <= 0) {
+        if (this.updates <= 0 || this.updates > this.rate) {
             this.updates = this.rate;
 
             this.game.bodies.push(new Body(
@@ -47,17 +44,13 @@ export default class Cannon {
             ));
         }
 
-        if (this.updates > this.rate) {
-            this.updates = this.rate;
-        }
-
         canvasDraw.drawLine(this.center, this.vectorToPoint());
 
         --this.updates;
     }
     select(point) {
         this.shootVector = new Vector(
-            7000,//pointUtils.distanceBetween(this.center, point),
+            7000,
             point === undefined ? Math.PI :
             Math.atan2(
                 this.center.y - point.y,
@@ -65,6 +58,12 @@ export default class Cannon {
             ) + Math.PI,
             "m/s"
         );
+
+        const scaledVector = this.shootVector.scale(10e3);
+        this.shootPointDelta = {
+            x: scaledVector.getDeltaX(),
+            y: scaledVector.getDeltaY()
+        };
     }
     deselect() {
         this.selected = false;
